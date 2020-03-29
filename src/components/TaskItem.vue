c8e56badb7017cd9ae161faeffd512bd3ba1634e
rayahen <kubablaszczak1@gmail.com>
2020-03-29 12:36:37 +0200

    Add lists of tasks and example tasks

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="taskItem">
+        <h1>{{taskList.name}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String
+    },
+    data() {
+        return{
+            taskList: [
+                {
+                    id: 1,
+                    name: "Poprawa edito"
+                },
+                {
+                    id: 2,
+                    name: "Testowy"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    .taskItem {
+        position: relative;
+        width: 100%;
+        border: 2px solid black;
+        height: 100px;
+    }
+</style>
