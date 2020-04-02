44aec0477144b17a1b03b7542d12e503de5d9dfa
rayahen <kubablaszczak1@gmail.com>
2020-04-02 21:52:37 +0200

    Add edit feature and date

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -7,10 +7,27 @@
         :class="{ isFavourite: favourite}"
         v-if="task.board_id == board_id"
         >
-          <h1>{{ task.name }}</h1>
-          <i class="fa fa-star-o" aria-hidden="true" 
+          <h1 v-if="!isEditing" class="taskTitle">{{ task.name }}</h1>
+
+          <input type="text" 
+          class="editTask"
+          v-model="task.name" 
+          v-if="isEditing"
+          >
+        
+            <div class="date">
+                {{ time }}
+            </div>
+          <i class="fa fa-pencil-square-o edit" 
+          aria-hidden="true" 
+          @click="handleEdit"
+          />
+
+          <i class="fa fa-star-o favourite" 
+          aria-hidden="true" 
           @click="toggleFavourite"
-          ></i>
+          />
+
         </div>
     </div>
 </template>
@@ -25,6 +42,8 @@ export default {
     data() {
         return{
             favourite: false,
+            isEditing: false,
+            time: null,  
             taskList: [
                 {
                     id: 1,
@@ -57,13 +76,51 @@ export default {
     methods: {
         toggleFavourite() {
                 this.favourite = !this.favourite
+        },
+        handleEdit() {
+            this.isEditing = !this.isEditing;
+        },
+        getDate() {
+            var currentTime = new Date().toLocaleString();
+            this.time = currentTime
         }
+    },
+    mounted() {
+        this.getDate()
     },
 }
 </script>
 
 <style lang="scss">
+    .taskTitle {
+        height: 40px;
+        margin: 10px;
+    }
 
+    .date {
+        height: auto;
+        width: auto;
+        font-size: 12px;
+        position: absolute;
+        top: 70%;
+        left: 5%;
+    }
+    .editTask {
+        position: absolute;
+        height: 40px;
+        width: 80%;
+        margin: 10px auto;
+        line-height: 100%;
+        background-color: transparent;
+        border: none;
+        color: white;
+        font-size: 20px;
+        border: 2px solid gray;
+        text-align: center;
+        &:focus {
+            border: 2px solid blue;
+        }
+    }
 
   .taskItem {
         position: relative;
@@ -74,14 +131,21 @@ export default {
         height: 100px;
         margin: 10px 0;
         font-size: 13px;
-        i {
+        i.favourite {
             position: absolute;
             top: 60%;
             right: 20px;
             font-size: 20px;
             height: 20px;
             cursor: pointer;
-
+        }
+        i.edit {
+            position: absolute;
+            top: 60%;
+            right: 50px;
+            font-size: 20px;
+            height: 20px;
+            cursor: pointer;
         }
   }
   .isFavourite {
